82d4ef42a0c5f1faf49f6640716c6e087eae041d
erivanstdio <erivanstdio@gmail.com>
2022-09-07 17:23:11 -0300

    feature: adding redux toolkit, store and counter slice

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './pages/Root.js';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Root from './pages/Root.js'
 import CountProvider from '../src/context/Count.js'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
 ReactDOM.render( 
   <React.StrictMode>
-    <CountProvider>
-      <Root />
-    </CountProvider>
+    <Provider store={ store }>
+      <CountProvider>
+        <Root />
+      </CountProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
