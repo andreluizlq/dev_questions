a1a9fdbb053b3c73ab1dd7ae0fdb3cf59e1bc0a6
andreluizlq <andreluizlq1@gmail.com>
2022-09-07 18:01:10 -0300

    Fix: removeing context

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Root from './pages/Root.js'
-import CountProvider from '../src/context/Count.js'
-import { Provider } from 'react-redux'
-import { store } from './redux/store'
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "./pages/Root.js";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
-ReactDOM.render( 
+ReactDOM.render(
   <React.StrictMode>
-    <Provider store={ store }>
-      <CountProvider>
-        <Root />
-      </CountProvider>
+    <Provider store={store}>
+      <Root />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
